b24c792adda41ae75fd9e48dbe4a7179f6e1cc10
nahin113 <nahinahmed113@gmail.com>
2026-05-21 17:19:44 +0600

    chore: configure MongoDB connection with

--- index.js
+++ index.js
@@ -14,3 +14,43 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+
+const uri = process.env.MONGODB_URI;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // comment this line for deploy
+    await client.connect();
+
+    const db = client.db("petora");
+    const destinationCollection = db.collection("pets");
+
+
+    // Send a ping to confirm a successful connection
+    // comment this line for deployment
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
