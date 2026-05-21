8183013e3c42c5ddc624d25e229b9566cc4fdaac
nahin113 <nahinahmed113@gmail.com>
2026-05-21 20:26:10 +0600

    feat: handle adoption requests with AdoptionRequest collection

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ async function run() {
 
     const db = client.db("petora");
     const petsCollection = db.collection("pets");
+    const adoptionCollection = db.collection("adoptionRequests");
 
     app.post("/petsData", async (req, res) => {
       const petData = req.body;
@@ -48,8 +49,6 @@ async function run() {
       const result = await petsCollection.find().toArray();
       res.json(result);
     });
-
-
 
     app.get("/myPets/:email", async (req, res) => {
       try {
@@ -85,7 +84,57 @@ async function run() {
       }
     });
 
+    app.get("/petsData/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await petsCollection.findOne(query);
 
+        if (!result) {
+          return res.status(404).json({ error: "Pet not found" });
+        }
+
+        res.status(200).json(result);
+      } catch (error) {
+        console.error("Error fetching pet details:", error);
+        res.status(500).json({ error: "server error" });
+      }
+    });
+
+    app.post("/adoptionRequests", async (req, res) => {
+      try {
+        const requestData = req.body;
+        const petRecord = await petsCollection.findOne({
+          _id: new ObjectId(requestData.petId),
+        });
+
+        if (!petRecord) {
+          return res
+            .status(404)
+            .json({ error: "Target pet listing not found" });
+        }
+
+        if (petRecord.ownerEmail === requestData.requesterEmail) {
+          return res.status(403).json({
+            acknowledged: false,
+            error:
+              "Action denied. You cannot submit an adoption request for your own listing.",
+          });
+        }
+
+        const result = await adoptionCollection.insertOne(requestData);
+        if (result.acknowledged) {
+          await petsCollection.updateOne(
+            { _id: new ObjectId(requestData.petId) },
+            { $inc: { requestsCount: 1 } }
+          );
+        }
+        res.status(201).json(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ acknowledged: false, error: "Error" });
+      }
+    });
 
     // Send a ping to confirm a successful connection
     // comment this line for deployment
